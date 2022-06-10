3b68cbe825a1de24947341bae23ec7718d01f3a2
Mansoor Omrani <mansoor.omrani@gmail.com>
2022-06-10 11:49:55 +0430

    Added sample messages

--- ProGitForProgrammers/ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Program.cs
@@ -6,10 +6,12 @@ using System.Threading.Tasks;
 
 namespace ProGitForProgrammers
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+			Console.WriteLine("I just added this in Visual Studio");
         }
     }
 }
